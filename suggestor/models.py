40ace60677686c7d7b9c9a17e2e58d93443a16da
Rohan Varma <rvarm1@gmail.com>
2017-11-08 03:17:40 -0800

    added some models

--- suggestor/models.py
+++ suggestor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ActivitySuggestion(models.Model):
+	activaty_text = models.CharField(max_length = 200)
+	date_suggested = models.DateTimeField('date suggested')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
