f9c885f3ffe5a57f185cd62a921d801559186703
Lucas Tecot <lucastecot@gmail.com>
2017-11-11 15:19:41 -0800

    Added endpoint for place recommender

--- mysite/placefindr/recommender_service.py
+++
@@ -1,6 +0,0 @@
-
-class RecommenderService:
-
-    def __init__(self):
-        self.place_recommender = PlaceRecommender()
-        # self.sharer = Sharer()
--- mysite/placefindr/views.py
+++ mysite/placefindr/views.py
@@ -5,8 +5,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
+from .place_recommender import PlaceRecommender
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the placefindr index.")
-    # return JsonResponse()
+def recommender(request):
+    # return HttpResponse("Hello, world. You're at the placefindr index.")
+    recommender = PlaceRecommender()
+    query_dict = request.GET.dict()
+    if 'pagetoken' in query_dict:
+        # Increment page token? query_result.next_page_token
+        pagetoken = query_dict['pagetoken']
+        places = recommender.get_places(pagetoken=pagetoken)
+    else:
+        if 'location' not in query_dict:
+            raise Http404('No location input.')
+        location = query_dict['location']
+        radius = query_dict['radius'] if 'radius' in query_dict else 8000  # 5 Miles
+        types = query_dict['types'] if 'types' in query_dict else []
+        places = recommender.get_places(location=location,
+                                       radius=radius,
+                                       types=types)
+    return JsonResponse(places.raw_response)
