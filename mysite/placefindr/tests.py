0a706b05b0d7d1a7d10a593a195bb47ae8ddb3f1
Rohan Varma <rvarm1@gmail.com>
2017-11-13 14:30:50 -0800

    fix import issue

--- mysite/placefindr/tests.py
+++ mysite/placefindr/tests.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-from .place_recommender import PlaceRecommender
+from place_recommender import PlaceRecommender
 
 class PlaceRecommenderTests(TestCase):
 
