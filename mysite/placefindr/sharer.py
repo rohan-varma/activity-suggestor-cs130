9ab95bac972535c2a1e8fff2d1a4d69cf71dd27e
John Song <johannsong@ucla.edu>
2017-11-12 16:21:17 -0800

    sharer docstring & error handling

--- mysite/placefindr/sharer.py
+++ mysite/placefindr/sharer.py
@@ -3,24 +3,42 @@ from twilio.rest import Client as TwilioClient
 from django.conf import settings
 
 def share_via_email(to_addr, place_id):
+    '''
+    Sends an email containing a Google Maps link for the location suggestion.
+
+    :param to_addr: string of the recipient's email address
+    :param place_id: a string that uniquely identifies a location to Google Maps
+    :return: 1 if email is sent, or 0 if not
+    '''
     # TODO: place_id or client-supplied json? When you put it that way though it's obvious
     # TODO [later]: personalize for sender (will need to authenticate user of website), and customize message depending on whether recipient is the same as sender (& always add to user profile)
     email = EmailMessage(
         subject='Your PlaceFindr Suggestion (Do Not Reply)',
         message='The following location has been shared to you:\n',
+        #TODO
         recipient_list=[ to_addr ],
         #connection=,
     )
 
-    return email.send(fail_silently=False)
+    return email.send(fail_silently=True)
 
 
 twilio_client = TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
 def share_via_text(to_number, place_id):
+    '''
+    Sends a text message (via Twilio) containing a Google Maps link for the location suggestion.
 
+    :param to_number: string of the recipient's phone number
+    :param place_id: a string that uniquely identifies a location to Google Maps
+    :return: <TODO>
+    '''
     message = twilio_client.messages.create(
         to=to_number, 
         from_="+15017250604",
         body="Hello from Python!")
+    
+    # TODO: Twilio's message status response is ridiculously complicated (https://www.twilio.com/docs/guides/how-to-confirm-delivery-in-python). Let's worry abot that after we've got a database.
+
+    return 1
 
